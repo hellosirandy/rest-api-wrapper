2a94c225a0c238bf9603dd7a49824acd07bf255e
hellosirandy <sac840711@gmail.com>
2019-05-15 16:47:20 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import queryString from 'query-string';
+
+export default class API {
+  constructor(url) {
+    this.url = url;
+  }
+
+  get = async (endpoint, token, params) => {
+    const url = params ? `${this.url}${endpoint}?${queryString.stringify(params)}` : `${this.url}${endpoint}`;
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    });
+    const parsedRes = await res.json();
+    if (!res.ok) {
+      throw parsedRes;
+    }
+    return parsedRes;
+  }
+
+  post = async (endpoint, token, body) => {
+    const res = await fetch(`${this.url}${endpoint}`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    });
+    const parsedRes = await res.json();
+    if (!res.ok) {
+      throw parsedRes;
+    }
+    return parsedRes;
+  }
+
+  put = async (endpoint, token, body) => {
+    const res = await fetch(`${this.url}${endpoint}`, {
+      method: 'PUT',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    });
+    return res.json();
+  }
+}
+
+// const test = () => {
+//   return 'hahaha'
+// }
+
+// export default test;
