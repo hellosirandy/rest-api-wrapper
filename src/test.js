5565d7a84f9c6bc33884f7308ff11ba755612874
Andy Chien <andy.chien@verizonmedia.com>
2020-05-11 23:44:05 -0700

    Remove unused script

--- src/test.js
+++
@@ -1,9 +0,0 @@
-import API from './index';
-
-const api = new API('http://localhost:5001/investment-portfolio-5b1fb/us-central1/api');
-
-api.get({
-  endpoint: '/user/portfolio',
-}).then((payload) => {
-  console.log(payload);
-});
